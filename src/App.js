bd2e2b332e3fa8ad29cdfa204d33c61fda1fdc0c
rodhrdz <rodrigo.hrzc@gmail.com>
2023-05-24 17:51:35 -0600

    falta estilizar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Loginbutton } from './components/Loginbutton';
 import { ProfileUser } from './components/ProfileUser';
 import { Logoutbutton } from './components/Logoutbutton';
 function App() {
+  const {isAuthenticated, isLoading} = useAuth0();
+  if(isLoading) return <h1>Loading...</h1>
   return (
     <div className="App">    
     <h1>Application</h1>  
-    <Loginbutton></Loginbutton>
+    { isAuthenticated ?    <Logoutbutton></Logoutbutton>:    <Loginbutton></Loginbutton>
+
+ }
     <ProfileUser></ProfileUser>
-    <Logoutbutton></Logoutbutton>
     
     </div>
   );
