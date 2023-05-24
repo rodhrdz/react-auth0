15125bc402c011124456baba200c8a48d31b7a4c
rodhrdz <rodrigo.hrzc@gmail.com>
2023-05-24 15:15:25 -0600

    se hace authentication con auth0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useAuth0 } from '@auth0/auth0-react';
 function App() {
+  const{loginWithRedirect} = useAuth0();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
+    <h1>Application</h1>  
+    <button onClick={() => loginWithRedirect()}>Login</button>
     </div>
   );
 }
